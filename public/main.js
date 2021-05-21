5ba213b2fb171f9c5f714f0baa948814a3361d61
Danny Le <le.dannyj@gmail.com>
2021-05-21 14:11:30 -0400

    Added Lizard and Spock logic

--- public/main.js
+++ public/main.js
@@ -2,23 +2,60 @@ function main() {
   const rock = '🪨'
   const paper = '📃'
   const scissor = '✂️'
+  const lizard = '🦎'
+  const spock = '🖖🏼'
+
   let p1 = ''
   let p2 = ''
 
   function gameLogic() {
+    // DRAW LOGIC
     if (p1 === p2) {
       window.alert('DRAW!')
-    } else if (p1 === rock && p2 === scissor) {
+    }
+
+    // PLAYER 1 LOGIC
+    else if (p1 === rock && p2 === scissor) {
+      window.alert('PLAYER 1 WINS')
+    } else if (p1 === rock && p2 === lizard) {
       window.alert('PLAYER 1 WINS')
     } else if (p1 === scissor && p2 === paper) {
       window.alert('PLAYER 1 WINS')
-    } else if (p1 == 'paper' && p2 == 'rock') {
+    } else if (p1 === scissor && p2 === lizard) {
       window.alert('PLAYER 1 WINS')
+    } else if (p1 === paper && p2 === rock) {
+      window.alert('PLAYER 1 WINS')
+    } else if (p1 === paper && p2 === spock) {
+      window.alert('PLAYER 1 WINS')
+    } else if (p1 === lizard && p2 === spock) {
+      window.alert('PLAYER 1 WINS')
+    } else if (p1 === lizard && p2 === paper) {
+      window.alert('PLAYER 1 WINS')
+    } else if (p1 === spock && p2 === rock) {
+      window.alert('PLAYER 1 WINS')
+    } else if (p1 === spock && p2 === scissor) {
+      window.alert('PLAYER 1 WINS')
+
+      // PLAYER 2 LOGIC
     } else if (p2 === rock && p1 === scissor) {
+      window.alert('PLAYER 2 WINS')
+    } else if (p2 === rock && p1 === lizard) {
       window.alert('PLAYER 2 WINS')
     } else if (p2 === scissor && p1 === paper) {
       window.alert('PLAYER 2 WINS')
-    } else if (p2 == paper && p1 == rock) {
+    } else if (p2 === scissor && p1 === lizard) {
+      window.alert('PLAYER 2 WINS')
+    } else if (p2 === paper && p1 === rock) {
+      window.alert('PLAYER 2 WINS')
+    } else if (p2 === paper && p1 === spock) {
+      window.alert('PLAYER 2 WINS')
+    } else if (p2 === lizard && p1 === spock) {
+      window.alert('PLAYER 2 WINS')
+    } else if (p2 === lizard && p1 === paper) {
+      window.alert('PLAYER 2 WINS')
+    } else if (p2 === spock && p1 === rock) {
+      window.alert('PLAYER 2 WINS')
+    } else if (p2 === spock && p1 === scissor) {
       window.alert('PLAYER 2 WINS')
     }
   }
@@ -55,6 +92,28 @@ function main() {
   document
     .querySelector('.team1 .scissor')
     .addEventListener('click', selectScissorOne)
+
+  //  LIZARD
+  function selectLizardOne(event) {
+    const teamOneLizard = document.querySelector('.team1 h3')
+    teamOneLizard.textContent = lizard
+    p1 = lizard
+    console.log(p1)
+  }
+  document
+    .querySelector('.team1 .lizard')
+    .addEventListener('click', selectLizardOne)
+
+  //  SPOCK
+  function selectSpockOne(event) {
+    const teamOneSpock = document.querySelector('.team1 h3')
+    teamOneSpock.textContent = spock
+    p1 = spock
+    console.log(p1)
+  }
+  document
+    .querySelector('.team1 .spock')
+    .addEventListener('click', selectSpockOne)
 
   //--------------------------------
 
@@ -95,6 +154,30 @@ function main() {
   document
     .querySelector('.team2 .scissor')
     .addEventListener('click', selectScissorTwo)
+
+  //  LIZARD
+  function selectLizardTwo(event) {
+    const teamTwoLizard = document.querySelector('.team2 h3')
+    teamTwoLizard.textContent = lizard
+    p2 = lizard
+    console.log(p2)
+    gameLogic()
+  }
+  document
+    .querySelector('.team2 .lizard')
+    .addEventListener('click', selectLizardTwo)
+
+  //  SPOCK
+  function selectSpockTwo(event) {
+    const teamTwoSpock = document.querySelector('.team2 h3')
+    teamTwoSpock.textContent = spock
+    p2 = spock
+    console.log(p2)
+    gameLogic()
+  }
+  document
+    .querySelector('.team2 .spock')
+    .addEventListener('click', selectSpockTwo)
 
   // RESET
 
